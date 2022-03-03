ae1115f3575c865981d88e161c04fa80f3fc0fbb
Babelpoint Ltd <krumbacher@gmail.com>
2022-03-03 11:40:56 +0000

    edited somemthing

--- src/index.js
+++ src/index.js
@@ -300,12 +300,8 @@ class App extends React.Component {
         super(props);
         this.state = {
             filterText: "",
-            
-            
-        
         }
-        this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
-        
+        this.handleFilterTextChange = this.handleFilterTextChange.bind(this); 
     }
 
 
@@ -321,8 +317,7 @@ class App extends React.Component {
                 <div>
                     <NavigationBar />
                     <SearchBar filterText={this.state.filterText}
-                        onFilterTextChange={this.handleFilterTextChange}
-                        
+                        onFilterTextChange={this.handleFilterTextChange}  
                     />
                     <br></br>
                     <Post />
@@ -345,43 +340,7 @@ class App extends React.Component {
     }
 
 
-const FILMS = [
-    {
-        "film_id": 1, "title": "ACADEMY DINOSAUR", "description": "A Epic Drama of a Feminist And a Mad Scientist who must Battle a Teacher in The Canadian Rockies",
-        "release_year": 2006, "rental_duration": 6, "rental_rate": 0.99, "length": 86, "replacement_cost": 20.99, "rating": "PG"
-    },
-    {
-        "film_id": 2, "title": "ACE GOLDFINGER", "description": "A Astounding Epistle of a Database Administrator And a Explorer who must Find a Car in Ancient China",
-        "release_year": 2006, "rental_duration": 3, "rental_rate": 4.99, "length": 48, "replacement_cost": 12.99, "rating": "G"
-    },
-    {
-        "film_id": 3, "title": "ADAPTATION HOLES", "description": "A Astounding Reflection of a Lumberjack And a Car who must Sink a Lumberjack in A Baloon Factory",
-        "release_year": 2006, "rental_duration": 7, "rental_rate": 2.99, "length": 50, "replacement_cost": 18.99, "rating": "NC-17"
-    },
-    {
-        "film_id": 4, "title": "AFFAIR PREJUDICE", "description": "A Fanciful Documentary of a Frisbee And a Lumberjack who must Chase a Monkey in A Shark Tank",
-        "release_year": 2006, "rental_duration": 5, "rental_rate": 2.99, "length": 117, "replacement_cost": 26.99, "rating": "G"
-    },
-    {
-        "film_id": 5, "title": "AFRICAN EGG", "description": "A Fast-Paced Documentary of a Pastry Chef And a Dentist who must Pursue a Forensic Psychologist in The Gulf of Mexico",
-        "release_year": 2006, "rental_duration": 6, "rental_rate": 2.99, "length": 130, "replacement_cost": 22.99, "rating": "G"
-    },
-    {
-        "film_id": 6, "title": "AGENT TRUMAN", "description": "A Intrepid Panorama of a Robot And a Boy who must Escape a Sumo Wrestler in Ancient China",
-        "release_year": 2006, "rental_duration": 3, "rental_rate": 2.99, "length": 169, "replacement_cost": 17.99, "rating": "PG"
-    },
-    {
-        "film_id": 7, "title": "AIRPLANE SIERRA", "description": "A Touching Saga of a Hunter And a Butler who must Discover a Butler in A Jet Boat",
-        "release_year": 2006, "rental_duration": 6, "rental_rate": 4.99, "length": 62, "replacement_cost": 28.99, "rating": "PG-13"
-    },
-    {
-        "film_id": 8, "title": "AIRPORT POLLOCK", "description": "A Epic Tale of a Moose And a Girl who must Confront a Monkey in Ancient India",
-        "release_year": 2006, "rental_duration": 6, "rental_rate": 4.99, "length": 54, "replacement_cost": 15.99, "rating": "R"
-    },
-]
-
-
 ReactDOM.render(
-    <App films = {FILMS}/>,
+    <App />,
     document.getElementById('root')
   );
